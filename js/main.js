49434ed8473ae70e300e5b1daf9a102826ab75a2
Valery Mashenets <webvalery.git@gmail.com>
2022-09-03 13:05:04 +0300

    add feedback modal

--- js/main.js
+++ js/main.js
@@ -17,42 +17,57 @@ btnCityMsk.addEventListener('click', (event) => {
   event.preventDefault();
   handlerCityMsk(event);
 });
+
 function handlerCityMsk(event) {
   btnCitySpb.classList.remove('home__city-btn__active');
   btnCityMsk.classList.add('home__city-btn__active');
   inputNumberPhone.innerHTML = '+7 (999) 999 99 99';
 }
+
 document.addEventListener('DOMContentLoaded', (event) => {
   handlerCitySpb();
 })
 
-// Модальное окно Консультации
+// Модальное окно КОНСУЛЬТАЦИЯ
 let consulPopupBg = document.querySelector('.consultation-popup-bg');
 let consulPopup = document.querySelector('.consultation-popup');
 let consulOpenPopupBtn = document.querySelector('.home-info__call');
 let consulClosePopupBtn = document.querySelector('.consultation-close-btn');
+let btnConsulFeedbackCall = document.querySelector('.consultation__feedback-call-btn');
 
 consulOpenPopupBtn.addEventListener('click', (event) => {
   event.preventDefault();
+  consulOpenPopup();
+})
+
+consulClosePopupBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  consulClosePopup();
+})
+
+document.addEventListener('click', (event) => {
+  if (event.target === consulPopupBg) {
+    consulClosePopup();
+  }
+})
+
+function consulOpenPopup() {
   consulPopupBg.classList.add('active');
   consulPopup.classList.add('active');
   consulPopup.style.transition = '0s';
   consulPopupAdaptive();
-})
-consulClosePopupBtn.addEventListener('click', (event) => {
-  event.preventDefault();
+}
+
+function consulClosePopup() {
   consulPopupBg.classList.remove('active');
   consulPopup.classList.remove('active');
   consulPopup.style.transition = '1s';
-})
-document.addEventListener('click', (event) => {
-  if (event.target === consulPopupBg) {
-    consulPopupBg.classList.remove('active');
-    consulPopup.classList.remove('active');
-    consulPopup.style.transition = '1s';
-  }
-})
+}
 
+btnConsulFeedbackCall.addEventListener('click', (event) => {
+  consulClosePopup();
+  openBackCallModal();
+});
 // Адаптив модального окна консультации
 let screenHeight = window.screen.height;
 let consulPopupHeight = consulPopup.offsetHeight;
@@ -81,5 +96,39 @@ function consulPopupAdaptive() {
   if (window.screen.width <= 661) {
     consulPopup.style.maxWidth = `350px`;
   }
+}
+
+// Модальное окно ОБРАТНЫЙ ЗВОНОК
+let backCallModalBg = document.querySelector('.back-call-modal-bg');
+let backCallModal = document.querySelector('.back-call-modal');
+let openBtnBackCallModal = document.querySelector('.home__contacts-call');
+let closeBtnBackCallModal = document.querySelector('.back-call-modal__btn-close');
+
+openBtnBackCallModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  openBackCallModal();
+})
+
+closeBtnBackCallModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  closeBackCallModal();
+})
+
+document.addEventListener('click', (event) => {
+  if (event.target === backCallModalBg) {
+    closeBackCallModal();
+  }
+})
+
+function openBackCallModal() {
+  backCallModalBg.classList.add('active');
+  backCallModal.classList.add('active');
+  backCallModalBg.style.transition = '0s';
+}
+
+function closeBackCallModal() {
+  backCallModalBg.classList.remove('active');
+  backCallModal.classList.remove('active');
+  backCallModalBg.style.transition = '1s';
 }
 
